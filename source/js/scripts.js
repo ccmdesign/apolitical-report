e5257265cb9121ca96e35a182e85f867ba1b7a96
Arthur A. Bergamaschi <arthur.bergamaschi@wowsolution.com>
2019-08-15 18:21:45 -0300

    :rocket: Add lock overlay.

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,6 @@
+const appVisibilityItemName = 'ar_f79b1d64a8d8b02b86d797765a6a1fe4'
+// localStorage.clear(appVisibilityItemName)
+
 function menuVisibility (elements, visibility) {
   if (visibility) {
     if (!elements.menuContainer.hasClass('menu-active')) {
@@ -10,12 +13,52 @@ function menuVisibility (elements, visibility) {
   }
 }
 
+function checkVisiblity(elements) {
+  if (localStorage.getItem(appVisibilityItemName)) {
+    elements.appWrapper.addClass('visible')
+    elements.lockWrapper.addClass('hidden')
+  } else {
+    elements.appWrapper.addClass('hidden')
+    elements.lockWrapper.addClass('visible')
+  }
+}
+
+function tryVisiblity(elements) {
+  if (!elements.lockMessage.hasClass('hidden')) {
+    elements.lockMessage.addClass('hidden')
+  }
+  const key = elements.lockInput.val()
+  if (key === 'ccm.design2019') {
+    elements.lockInput.val('')
+    localStorage.setItem(appVisibilityItemName, true)
+    elements.appWrapper.removeClass('hidden')
+    elements.lockWrapper.removeClass('visible')
+    elements.appWrapper.addClass('visible')
+    elements.lockWrapper.addClass('hidden')
+  } else {
+    elements.lockMessage.text('Incorrect key.')
+    elements.lockMessage.removeClass('hidden')
+    if (!elements.appWrapper.hasClass('hidden')) elements.appWrapper.addClass('hidden')
+    if (!elements.lockWrapper.hasClass('visible')) elements.lockWrapper.addClass('visible')
+  }
+}
+
 $(document).ready(function () {
   const elements = {
     body: $('body'),
+    appWrapper: $('#app'),
+    lockWrapper: $('#lock-overlay'),
+    lockInput: $('#lock-input'),
+    lockMessage: $('.lock-overlay__message'),
     menuContainer: $('.menu-container'),
     homepagePredictionList: $('.homepage-prediction-list')
   }
+
+  checkVisiblity(elements)
+
+  $('#lock-button').click(function () {
+    tryVisiblity(elements)
+  })
   
   $('.js-menu-button').click(function () {
     menuVisibility(elements, !elements.menuContainer.hasClass('menu-active'))
